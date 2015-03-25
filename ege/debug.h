a6fba88a18190cb28ee747ffe30a8af7f615e503
Edouard DUPIN <yui.heero@gmail.com>
2015-03-25 21:14:12 +0100

    [DEV] update Log interafce

--- ege/debug.h
+++ ege/debug.h
@@ -14,16 +14,7 @@
 namespace ege {
 	int32_t getLogId();
 };
-// TODO : Review this problem of multiple intanciation of "std::stringbuf sb"
-#define EGE_BASE(info,data) \
-	do { \
-		if (info <= etk::log::getLevel(ege::getLogId())) { \
-			std::stringbuf sb; \
-			std::ostream tmpStream(&sb); \
-			tmpStream << data; \
-			etk::log::logStream(ege::getLogId(), info, __LINE__, __class__, __func__, tmpStream); \
-		} \
-	} while(0)
+#define EGE_BASE(info,data)  TK_LOG_BASE(ege::getLogId(),info,data)
 
 #define EGE_CRITICAL(data)      EGE_BASE(1, data)
 #define EGE_ERROR(data)         EGE_BASE(2, data)
